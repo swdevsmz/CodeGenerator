1d6cecff9554cf5bb627039f5d686b623845bf73
swdevsmz <swdev.smz@gmail.com>
2018-06-24 20:38:49 +0900

    Delete Doshirase.sql

--- CodeGenerator/sql/Doshirase.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Doshirase` (
-  `guideid` int(11) NOT NULL AUTO_INCREMENT,
-  `UserShubetu` int(11) NOT NULL,
-  `testmode` int(2) NOT NULL,
-  `guide` text CHARACTER SET utf8 NOT NULL,
-  `information` text CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`guideid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
