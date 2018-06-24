1c38ce15af7a1e6ed01f0af6c209cf1a50363a2e
swdevsmz <swdev.smz@gmail.com>
2018-06-24 20:39:57 +0900

    Delete Mriyoukigyouhatsuban.sql

--- CodeGenerator/sql/Mriyoukigyouhatsuban.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Mriyoukigyouhatsuban` (
-  `RiyoukigyouCD` varchar(10) NOT NULL,
-  `RenbanName` varchar(50) NOT NULL,
-  `Settoji` varchar(3) DEFAULT NULL,
-  `Renban` int(11) DEFAULT NULL,
-  PRIMARY KEY (`RiyoukigyouCD`,`RenbanName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
